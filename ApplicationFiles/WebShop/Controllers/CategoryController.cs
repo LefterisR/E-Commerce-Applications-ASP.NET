1b772725267033680c4908da29709d3f7ab434d2
LefterisR <162315856+LefterisR@users.noreply.github.com>
2025-04-05 22:51:02 +0300

    Category Controller Index Bootstrap Mint css

---
+++ ApplicationFiles/WebShop/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CategoryController(ApplicationDbContext db) : Controller
+    {
+
+        private readonly ApplicationDbContext _db = db;
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
--- ApplicationFiles/WebShop/Data/ApplicationDbContext.cs
+++ ApplicationFiles/WebShop/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Models;
 
 namespace WebShop.Data
 {
@@ -7,6 +8,18 @@ namespace WebShop.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                
+                );
         }
     }
 }
